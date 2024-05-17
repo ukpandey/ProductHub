322bb1458a7f0524ce99fbbd29f98dafc04cb73e
ukpandey <ujjwalpandey223@gmail.com>
2024-05-18 00:09:52 +0530

    Added Logging Aspects for Controller and Service

---
+++ src/main/java/com/example/ProductHub/MyProductsServiceLoggingAspect.java
@@ -0,0 +1,33 @@
+package com.example.ProductHub;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyProductsServiceLoggingAspect {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(MyProductsConrollerLoggingAspect.class);
+	
+	@Before("execution(* com.example.ProductHub.Service.MyProductsService.*(..))")
+    public void logBeforeMethod(JoinPoint joinPoint) {
+        LOGGER.info("Entering method: " + joinPoint.getSignature().getName() + " with arguments: " + joinPoint.getArgs());
+    }
+
+    @AfterReturning(pointcut = "execution(* com.example.ProductHub.Service.MyProductsService.*(..))", returning = "result")
+    public void logAfterReturningMethod(JoinPoint joinPoint, Object result) {
+        LOGGER.info("Exiting method: " + joinPoint.getSignature().getName() + " with result: " + result);
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.example.ProductHub.Service.MyProductsService.*(..))", throwing = "exception")
+    public void logAfterThrowingMethod(JoinPoint joinPoint, Throwable exception) {
+        LOGGER.info("Exception in method: " + joinPoint.getSignature().getName() + " with message: " + exception.getMessage());
+    }
+
+}
